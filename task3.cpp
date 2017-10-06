d04faaaf45e4470cbe232fd7e5a135d26d0aa062
Matthew Francis <matthew_francis@outlook.com>
2017-10-06 14:24:45 -0400

    Inital progess task 3

--- task3.cpp
+++ task3.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <openssl/evp.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
 
-int main() {
+using namespace std;
+
+int main(int argc, char *argv[]) {
     EVP_MD_CTX *ctx;
     EVP_MD_CTX_init(ctx);
 
-    std::cout << "Hello, World!" << std::endl;
+    ifstream inFile;
+    string unhashed_file;
+    string line;
+    string filename;
+
+    if (argc != 2) {
+       cout << "Incorrect number of arguments sent the program, try adding a file name?";
+        return 1;
+    }
+
+
+    filename = argv[1];
+    cout << "Filename given: " + filename << "\n";
+
+    inFile.open(filename);
+    if (inFile.fail()) {
+        cout << "Unable to open file";
+    }
+
+    while (getline(inFile, line)) {
+        unhashed_file.append(line);
+    }
+
+
+    inFile.close();
+
+    //cout << "Hello, World!" << std::endl;
     return 0;
 }
