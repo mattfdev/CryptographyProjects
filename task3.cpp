128a5e033349fd411cd24a17a0625efe5f5becce
Matthew Francis <matthew_francis@outlook.com>
2017-10-07 13:24:15 -0400

    Finish task 3: md5 hashing a file.

--- task3.cpp
+++ task3.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <openssl/evp.h>
+#include <openssl/md5.h>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    EVP_MD_CTX *ctx;
-    EVP_MD_CTX_init(ctx);
-
+    EVP_MD_CTX ctx;
+    EVP_MD_CTX_init(&ctx);
+    const EVP_MD *md;
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned int md_len;
     ifstream inFile;
     string unhashed_file;
     string line;
@@ -21,22 +25,29 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-
+    //Read specified file in, and feed to md5 digest function
     filename = argv[1];
     cout << "Filename given: " + filename << "\n";
-
     inFile.open(filename);
     if (inFile.fail()) {
         cout << "Unable to open file";
     }
-
-    while (getline(inFile, line)) {
-        unhashed_file.append(line);
-    }
-
-
+    md = EVP_md5();
+    EVP_DigestInit_ex(&ctx, md, nullptr);
+    stringstream buffer;
+    // WOrkfing
+    buffer << inFile.rdbuf();
+    //cout << buffer.str().c_str();
+    char* contents = strdup(buffer.str().c_str()) ;
+    EVP_DigestUpdate(&ctx, contents, strlen(contents));
     inFile.close();
 
-    //cout << "Hello, World!" << std::endl;
+    //Print out final digest
+    EVP_DigestFinal(&ctx, md_value, &md_len);
+    printf("Digest is: ");
+    for(int i = 0; i < md_len; i++)  {
+        printf("%02x", md_value[i]);
+    }
+
     return 0;
 }
