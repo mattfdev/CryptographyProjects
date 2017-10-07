d7f8adb8228c5b443305004e99acbdd347817a75
Matthew Francis <matthew_francis@outlook.com>
2017-10-07 18:24:36 -0400

    Include altered string hashing in task 3

--- task3.cpp
+++ task3.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <openssl/evp.h>
-#include <openssl/md5.h>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    EVP_MD_CTX ctx;
+    EVP_MD_CTX ctx, ctx2;
     EVP_MD_CTX_init(&ctx);
+    EVP_MD_CTX_init(&ctx2);
     const EVP_MD *md;
     unsigned char md_value[EVP_MAX_MD_SIZE];
     unsigned int md_len;
-    ifstream inFile;
-    string unhashed_file;
-    string line;
+    fstream inFile;
     string filename;
+    stringstream buffer;
 
     if (argc != 2) {
        cout << "Incorrect number of arguments sent the program, try adding a file name?";
@@ -27,27 +29,41 @@ int main(int argc, char *argv[]) {
 
     //Read specified file in, and feed to md5 digest function
     filename = argv[1];
-    cout << "Filename given: " + filename << "\n";
+    cout << "Filename given: " + filename << endl;
     inFile.open(filename);
     if (inFile.fail()) {
         cout << "Unable to open file";
+        return 1;
     }
     md = EVP_md5();
     EVP_DigestInit_ex(&ctx, md, nullptr);
-    stringstream buffer;
-    // WOrkfing
     buffer << inFile.rdbuf();
-    //cout << buffer.str().c_str();
-    char* contents = strdup(buffer.str().c_str()) ;
-    EVP_DigestUpdate(&ctx, contents, strlen(contents));
-    inFile.close();
+    char* contents = strdup(buffer.str().c_str());
+    size_t contents_size = strlen(contents);
+    EVP_DigestUpdate(&ctx, contents, contents_size);
 
-    //Print out final digest
+
+    //Print out initial file md5 digest
     EVP_DigestFinal(&ctx, md_value, &md_len);
-    printf("Digest is: ");
+    cout << "MD5 hash for input file [H1] −−> ";
     for(int i = 0; i < md_len; i++)  {
         printf("%02x", md_value[i]);
     }
 
+    // Alter the last byte(char) of the file contents, simpler than bit flipping.
+    char contents_bit_shifted[contents_size];
+    strncpy(contents_bit_shifted, contents, contents_size);
+    cout << endl << "Flipping Last bit ..." << endl;
+    contents_bit_shifted[contents_size - 1] = ' ';
+    EVP_DigestInit_ex(&ctx2, md, nullptr);
+    EVP_DigestUpdate(&ctx2, contents_bit_shifted, strlen(contents_bit_shifted));
+    EVP_DigestFinal(&ctx2, md_value, &md_len);
+    cout << "MD5 hash for input file [H2] −−> ";
+    for(int i = 0; i < md_len; i++)  {
+        printf("%02x", md_value[i]);
+    }
+    cout << endl << "These two files have 8 diffrent bits (1 byte)";
+
+    inFile.close();
     return 0;
 }
