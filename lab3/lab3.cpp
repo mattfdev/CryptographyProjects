8b9db096d82f359de9e50b2d90373d0654285f2d
r2huq <reaz.huq@ryerson.ca>
2017-11-09 13:14:58 -0500

    added check for valid key, added iteration fxn for key.

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <bitset>
 
 
 using namespace std;
@@ -58,17 +59,60 @@ vector<bitset<12>> convert_binary_strings_to_blocks(vector<string> bit_strings) 
     return encryption_blocks;
 }
 
+bitset<8> get_round_key(bitset<9> ekey, int n)
+{
+
+    bitset<8> roundKey;
+    n = n - 1; // Key returns the nth digit onwards
+    for (int i = 0; i < 9 ; ++i ) {
+        roundKey[i] = ekey[(n+i)%9];
+    }
+    return roundKey;
+}
+
+bitset<8> expansion(bitset<6> bit_strings) {
+    bitset<8> oput;
+    oput[0] = bit_strings[0];
+    oput[1] = bit_strings[1];
+    oput[2] = bit_strings[3];
+    oput[3] = bit_strings[2];
+    oput[4] = bit_strings[3];
+    oput[5] = bit_strings[2];
+    oput[6] = bit_strings[4];
+    oput[7] = bit_strings[5];
+    return oput;
+}
+
+
+
+bitset<8> encrypt(bitset<6> bit_strings, bitset<9> ekey ) {
+    bitset<8> encrypted_stuff = expansion(bit_strings);
+    encrypted_stuff = encrypted_stuff ^= ekey;
+
+}
+
 int main(int argc, char *argv[]) {
 
     vector<string> bit_strings;
     vector<bitset<12>> encryption_blocks;
 
-    if (argc != 2) {
-        cout << "Incorrect number of arguments sent the program, please supply some plaintext for encryption" << endl;
+    if (argc != 3) {
+        cout << "Incorrect number of arguments sent the program, please supply some plaintext and a key of length 9 for encryption" << endl;
         return 1;
     }
 
     string plaintext =  argv[1];
+    string key = argv[2];
+    if (key.find_first_not_of("01") != std::string::npos) {
+        cout << "Illegal key inputted, please input another key, consisting wholly of binary digits" << endl;
+        return false;
+    }
+
+    if (key.length() != 9) {
+        cout << "Please enter a valid binary key of length 9" << endl;
+        return 1;
+    }
+    bitset<9> encryption_key(key);
 
     cout << plaintext << endl;
     bit_strings = convert_to_binary(plaintext);
